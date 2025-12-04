34041cb9fd6899c253882161bffd1e1a60e7ef81
Bart Veneman <bartveneman@gmail.com>
2025-12-04 21:20:45 +0100

    mark parser as external

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { resolve } from "path"
-import { defineConfig } from "vite"
-import { codecovVitePlugin } from "@codecov/vite-plugin"
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { codecovVitePlugin } from '@codecov/vite-plugin'
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.js"),
-      formats: ['es']
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['css-tree'],
-    },
-  },
-  plugins: [
-    codecovVitePlugin({
-      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: "cssLayerTree",
-      uploadToken: process.env.CODECOV_TOKEN,
-    }),
-  ],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.js'),
+			formats: ['es'],
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ['@projectwallace/css-parser'],
+		},
+	},
+	plugins: [
+		codecovVitePlugin({
+			enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+			bundleName: 'cssLayerTree',
+			uploadToken: process.env.CODECOV_TOKEN,
+		}),
+	],
 })
